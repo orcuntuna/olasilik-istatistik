cc92e2088a427dfd01a3fde4739d8b17b4b95269
orcuntuna <turboindirblog@gmail.com>
2019-03-07 14:08:27 +0300

    varyans, standart sapma, carpitlik, basiklik

---
+++ python/basiklik.py
@@ -0,0 +1,19 @@
+adet = int(input("Adet giriniz: "))
+dizi = []
+toplamlar = 0
+
+for i in range(adet):
+    dizi.append(int(input(str(i+1) + ". sayi: ")))
+
+for x in range(adet):
+    sayi = dizi[x]
+    toplamlar += sayi
+
+aritmatik_ort = toplamlar // adet
+
+ust = 0
+for i in dizi:
+    ust += (aritmatik_ort-i) ** 4
+
+sonuc = (ust/(adet-1))
+print(sonuc)
